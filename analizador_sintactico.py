25937bd34d832571b1bd3effd2b157cb9f9d9b75
KevinBarboza <Kevin.barboza@evotek.com.mx>
2023-05-23 20:13:10 -0600

    correcion de matriz sintactica

--- analizador_sintactico.py
+++ analizador_sintactico.py
@@ -47,6 +47,7 @@ matriz_sintactica = [
     ['e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 91, 92, 93, 94, 96, 95, 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e']
 ]
 
+
 lista_simbolos = ['{{','}}',':','((','))',';',',','{','}','(',')','[',']','|||','&&&','~','/+\\','/-\\','/*\\','/\\','<*<','>*>','<*=','>*=','=*=','==','=','identificador','numero','texto','int','string','real','logical','list','funcion','principal','regresar','mientras','hacer','ciclo','repeat','si','para',
                 'dentro','imprimir','longitud','entero','decimal','entrada','absoluto','cadena','potencia','redondear','sumar','minimo','maximo','elseif','else','rango','eof']
 
@@ -56,7 +57,91 @@ lista_no_terminales = ['PROGRAMA','DECLARACIONVARIABLES','VARIABLES','LISTAVARIA
 producciones = {
     1: 'DECLARACIONVARIABLES MAIN FUNCIONES',
     2: '{{ VARIABLES }}',
-    3: 'TIPO : LISTAVARIABLES ; VARIABLES'
+    3: 'TIPO : LISTAVARIABLES ; VARIABLES',
+    4: 'nulo',
+    5: 'identificador LISTAVARIABLESPRIMA',
+    6: ', LISTAVARIABLES',
+    7: 'nulo',
+    8: 'int',
+    9: 'string',
+    10: 'real',
+    11: 'logical',
+    12: 'list',
+    13: 'FUNCION FUNCIONES',
+    14: 'nulo',
+    15: 'funcion identificador PARAMETROS BLOQUE RETURN',
+    16: 'principal BLOQUE',
+    17: '(( VARIABLES ))',
+    18: 'regresar LISTARETURN ;',
+    19: 'identificador LISTARETURNPRIMA',
+    20: ', LISTARETURN',
+    21: 'nulo',
+    22: '{ ESTATUTOS }',
+    23: 'ESTATUTO ; ESTATUTOS',
+    24: 'identificador = LISTA',
+    25: 'mientras BOOLEXP BLOQUE',
+    26: 'hacer BLOQUE ciclo BOOLEXP',
+    27: 'repeat BLOQUE ciclo BOOLEXP',
+    28: 'si BOOLEXP BLOQUE ELSEIF ELSE',
+    29: 'para identificador dentro RANGO BLOQUE',
+    30: 'imprimir ( VARIABLESIMPRIMIR )',
+    31: '[ LISTAELEMENTOS ]',
+    32: 'EXPARITM',
+    33: 'FUNCION_BUILT_IN',
+    34: 'identificador { LISTAPARAMETROS }',
+    35: 'numero LISTAPRIMA',
+    36: 'ELEMENTO_TEXTOSPRIMA',
+    37: 'ELEMENTO_NUMEROSPRIMA',
+    38: ': numero',
+    39: 'nulo',
+    40: 'texto ELEMENTO_TEXTOSPRIMA',
+    41: 'nulo',
+    42: 'texto ELEMENTO_NUMEROSPRIMA',
+    43: 'nulo',
+    44: 'longitud ( identificador )',
+    45: 'entero ( identificador )',
+    46: 'decimal ( identificador )',
+    47: 'entrada ( )',
+    48: 'absoluto ( EXPARITM )',
+    49: 'cadena ( EXPARITM )',
+    50: 'potencia ( EXPARITM )',
+    51: 'redondear ( EXPARITM )',
+    52: 'sumar ( identificador )',
+    53: 'minimo ( identificador )',
+    54: 'maximo ( identificador )',
+    55: 'identificador VARIABLESPRIMA',
+    56: 'texto VARIABLESPRIMA',
+    57: ', VARIABLESIMPRIMIR',
+    58: 'nulo',
+    59: 'elseif BOOLEXP BLOQUE ELSEIF',
+    60: 'nulo',
+    70: '||| BOOLTERM BOOLEXP_PRIMA',
+    71: 'nulo',
+    72: 'BOOLFACTOR BOOLTERM_PRIMA',
+    73: '&&& BOOLFACTOR BOOLTERM_PRIMA',
+    74: 'nulo',
+    75: '~ BOOLFACTOR',
+    76: '( RELTERM RELTERMP )',
+    77: 'RELTERM RELTERMP',
+    78: 'OPERADOR RELTERM',
+    79: 'nulo',
+    80: 'EXPARITM',
+    81: 'TERMINO EXPPRIMA',
+    82: '/+\ TERMINO EXPPRIMA',
+    83: '/-\ TERMINO EXPPRIMA',
+    84: 'nulo',
+    85: 'FACTOR TERMPRIMO',
+    86: '/*\ FACTOR TERMPRIMO',
+    87: '/\ FACTOR TERMPRIMO',
+    88: 'nulo',
+    89: 'identificador',
+    90: 'numero',
+    91: '<*<',
+    92: '>*>',
+    93: '<*=',
+    94: '>*=',
+    95: '==',
+    96: '=*=',
 }
 
 lista_inicial = ['eof','PROGRAMA']
@@ -66,29 +151,37 @@ def obtener_columna(token):
         columna = lista_simbolos.index(token)
         return columna
     except ValueError:
-        if(caracter.isalpha() == True):
+        if(token.startswith('@') == True): #identificador
             return 27
-        elif(caracter.isdigit() == True):
+        elif(token.isdigit() == True): #numero 
             return 28
-        elif(caracter.isalpha() == True):
+        elif((token.startswith('"') == True) or (token.startswith("'"))): #texto
             return 29
         else:
             print('el caracter no existe en la lista')
+            return -1
 
 
-def obtener_produccion(numero_produccion):
+# def obtener_produccion(numero_produccion):
 
-    pass
+#     pass
 
 
 def main():
     renglon = 0 
-    while(lista_tokens[0] == 'eof'):
+    while(lista_tokens(len) != 0):
         token = lista_tokens[0]
-        columna = obtener_columna(token)
-        # buscar en la matriz sintactica la produccion a sustituir
-        numero_produccion = matriz_sintactica[renglon][columna]
-        produccion = obtener_produccion(numero_produccion)
+        simbolo_stack = lista_inicial[-1]
+        if (token == simbolo_stack):
+            # eliminan de ambas listas el simbolo y el token
+            pass
+        else:
+            renglon = obtener_renglon(simbolo_stack)
+            columna = obtener_columna(token)
+            # buscar en la matriz sintactica la produccion a sustituir
+            numero_produccion = matriz_sintactica[renglon][columna]
+            # produccion = obtener_produccion(numero_produccion)
+
 
 
 if __name__ == "__main__":
@@ -96,3 +189,4 @@ if __name__ == "__main__":
     analizador_lexico.main()
     print('tokens')
     pprint.pprint(tokens)
+    # print('lista de tokens: ', lista_tokens)
